c876346d1e3e8e350e23b8a58ebab235aee3f000
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-24 19:23:57 -0300

    Iniciando o Desafio das Estruturas Condicionais

--- Desafios/EstruturasCondicionais.js
+++ Desafios/EstruturasCondicionais.js
@@ -1,2 +1,20 @@
 // Desafio de Estruturas de Variáveis
 // Criando um programa para calcular o valor de uma viagem
+
+// O programa conta com 5 variáveis. Sendo elas:
+// 1- Preço do Etanol
+// 2- Preço da Gasolina
+// 3- O tipo de combustivel que está no seu carro
+// 4- Gasto médio do combustível em KM
+// 5- Distancia em KM da viagem
+
+const precoEtanol = 4.80;
+const precoGasolina = 6.53;
+const tipoDeCombustivel = "Gasolina"; // meu carro utiliza gasolina
+const consumoMedioKmPorLitro = 12; // o carro faz 12km por litro
+const distanciaKm = 500; // distancia da viagem em km
+
+//Cálculo
+const litrosNecessarios = distanciaKm / consumoMedioKmPorLitro;
+
+let custoTotal;
