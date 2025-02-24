d480c1bd508aaf4d6e7bd6d7c524606235f8f171
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-24 20:47:18 -0300

    Exercicio 1 - Média de Notas

---
+++ Exercicios/MediaDeNotas.js
@@ -0,0 +1,19 @@
+// 1) Faça um algoritmo que, dado as 3 notas tiradas por 
+// um aluno em 1 semestre da faculdade, calcule e imprima a 
+// sua media e a sua classificação conforme a tabela abaixo
+
+/*
+    Média = (nota 1 + nota 2 + nota 3) / 3;
+
+    Classificação:
+    - Média menor que 5, reprovado;
+    - Média entre 5 e 7, recuperação;
+    - Média acima de 7, passou de semestre;
+*/
+
+const nota1 = 5.9;
+const nota2 = 8.4;
+const nota3 = 6.7;
+
+let media;
+
