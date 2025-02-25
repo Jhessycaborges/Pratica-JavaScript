95f13d9a5bb9f6a74ab077bd4144d0aaeee3d18e
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-24 21:55:36 -0300

    Tabela IMC

---
+++ Exercicios/CalculosIMC.js
@@ -0,0 +1,12 @@
+// Fórmula do IMC:
+// IMC = peso / (altura * altura)
+
+//Elabore um algoritmo que, dado o peso e a altura de um adulto,
+// mostre sua condição de acordo com a tabela abaixo.
+
+// IMC em adultos. Condição:
+// Abaixo de 18.5 = Abaixo do peso;
+// Entre 18.5 e 25 = Peso normal;
+// Entre 25 e 30 = Acima do peso;
+// Entre 30 e 40 = Obesidade;
+// Acima de 40 = Obesidade morbida;
