2b59fa63283f89dcc2c4fe9bbda44543645717c7
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-24 23:00:52 -0300

    Correção de operador errado no console do Else

--- Exercicios/ValorProduto.js
+++ Exercicios/ValorProduto.js
@@ -20,7 +20,7 @@ const desconto4 = 0.10;
 // Preço do produto
 const precoProduto = 379;
 
-let formaDePagamento = 'Cartão 2x';
+let formaDePagamento = 'Cartão 3x ou mais';
 
 if (formaDePagamento === formaDePag1) {
     console.log(precoProduto - (precoProduto * desconto1));
