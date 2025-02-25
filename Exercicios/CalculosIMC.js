ac8853ea802f3f4125efaae85b3fe769b7660be2
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-24 22:02:13 -0300

    Exercício IMC correto

--- Exercicios/CalculosIMC.js
+++ Exercicios/CalculosIMC.js
@@ -10,3 +10,21 @@
 // Entre 25 e 30 = Acima do peso;
 // Entre 30 e 40 = Obesidade;
 // Acima de 40 = Obesidade morbida;
+
+const paciente = 'Jhessyca';
+const peso = 73;
+const altura = 1.74;
+
+const IMC = peso / (altura * altura);
+
+if (IMC < 18.5) {
+    console.log('Abaixo do peso');
+} else if (IMC >= 18.5 && IMC <= 25) {
+    console.log('Peso normal');
+} else if (IMC >= 25 && IMC <= 30) {
+    console.log('Acima do peso');
+} else if (IMC >= 30 && IMC <= 40) {
+    console.log('Obesidade');
+} else {
+    console.log('Obesidade morbida');
+}
