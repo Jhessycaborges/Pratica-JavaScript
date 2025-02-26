23833f1ad3e16dddd86f5648a3c6ca4616f1f9d1
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-25 23:39:38 -0300

    Atualização explicativa

---
+++ Aulas/Functions.js
@@ -0,0 +1,26 @@
+function teste() {
+    console.log('Hello World');
+}
+
+teste();
+
+// Exemplo detalhado de como funciona uma Function
+// Function = Bloco de códigos que serão executados quando o programa for rodado
+// Funções podem receber parâmetros e retornar um valor
+// Parâmetros = Variáveis declaradas na Function
+// Retorno = Valor retornado pela Function
+
+function soma(n1, n2) {
+    return n1 + n2
+} 
+
+console.log(soma(2, 2));
+
+// No caso acima, o parâmetro(variavel) n1 recebe o vallor 2
+// e o parâmetro(variavel) n2 recebe o vallor 2
+// O retorno da Function soma(n1, n2) recebe o valor 4
+// Porque se n1 + n2 = 2 + 2 = 4
+
+// Em que situações a Function deve ser utilizada?
+// Quando queremos que o códigos sejam reutilizados
+// em múltiplos locais sem precisar repetir o códigos
