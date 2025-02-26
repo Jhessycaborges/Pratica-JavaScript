181bb1cca3fd9c298bb2c10f7fb56de2fa162d8f
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-25 23:52:07 -0300

    Atualização +Didática

--- Aulas/Functions.js
+++ Aulas/Functions.js
@@ -1,11 +1,26 @@
+// Function sem retorno TEM parametros
 function MeuNome(name) {
     console.log('Seu nome é ' + name);
 }
 
 MeuNome('Jhessyca');
 
+// function nomeQualquer(parametro) {
+//      console.log(parametro); ou
+//      console.log('Qualquer coisa relacionado ao nomeQualquer ' + parametro);
+// }
+
+
+/*---------------*/
+/*---------------*/
+
+// Function com retorno NÃO tem parametros
 function MeuNome2() {
     return 'Jhessyca';
 }
 
 console.log(MeuNome2());
+
+// function nomeQualquer2() {
+//    return 'Qualquer coisa que quiser que retorne no console';
+// }
