365b54d6ea96f06be8e4f9b9d9927051718ddbc3
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-27 11:56:45 -0300

    Atualizaçao

--- Aulas/ExemploPraticoFunctions.js
+++ Aulas/ExemploPraticoFunctions.js
@@ -1,7 +1,21 @@
 // Exercício - Calcular o IMC
 
 function calcularIMC(peso, altura) {
-    return peso / Math.pow(altura, 2) // peso : altura ao quadrado
+    return peso / Math.pow(altura, 2); // peso : altura ao quadrado
+}
+
+function classificarIMC(IMC) {
+    if (IMC < 18.5) {
+        return 'Abaixo do peso';
+    } else if (IMC >= 18.5 && IMC <= 25) {
+        return 'Peso normal';
+    } else if (IMC >= 25 && IMC <= 30) {
+        return 'Acima do peso';
+    } else if (IMC >= 30 && IMC <= 40) {
+        return 'Obesidade';
+    } else {
+        return 'Obesidade morbida';
+    }
 }
 
 const paciente = 'Jhessyca';
@@ -9,16 +23,4 @@ const peso = 72;
 const altura = 1.74;
 
 const IMC = calcularIMC(peso, altura);
-console.log(IMC.toFixed(2))
-
-if (IMC < 18.5) {
-    console.log('Abaixo do peso');
-} else if (IMC >= 18.5 && IMC <= 25) {
-    console.log('Peso normal');
-} else if (IMC >= 25 && IMC <= 30) {
-    console.log('Acima do peso');
-} else if (IMC >= 30 && IMC <= 40) {
-    console.log('Obesidade');
-} else {
-    console.log('Obesidade morbida');
-}
+console.log(classificarIMC(IMC));
