21c606131f851040430d07ad1d4ff1220828ad3f
jhessycaborges <jhessyca.borgez@outlook.com>
2025-02-27 13:13:49 -0300

    Atualização

--- Exercicios/MeuNomeFunctions.js
+++ Exercicios/MeuNomeFunctions.js
@@ -1,4 +1,18 @@
-function EscrevaMeuNome(name) {
-    console.log('Meu nome é ' + name);
+//com parâmetro
+function escrevaMeuNome(nome) {
+    console.log('Meu nome é ' + nome)
 }
-EscrevaMeuNome('Jhessyca');
+
+escrevaMeuNome('Jhessyca');
+
+function VerificadorDeIdade(idade) {
+    if (idade > 18) {
+        console.log('Maior de idade');
+    } else {
+        console.log('Menor de Idade');
+    }
+}
+
+VerificadorDeIdade(); //acrescentar o parametro para verificar a idade
+
+//sem parâmetro
